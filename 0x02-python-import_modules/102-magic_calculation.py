309c7eaad13abbc47de1efa460e3be39acc813b6
simoonge1 <simoimagine@gmail.com>
2023-10-05 03:39:26 -0700

    py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,15 +1,21 @@
-#!/usr/bin/python3
-
-
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
+  """Performs a magic calculation on two numbers.
 
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
+  Args:
+    a: The first number.
+    b: The second number.
 
-    else:
-        return(sub(a, b))
+  Returns:
+    The result of the magic calculation.
+  """
+
+  add = magic_calculation_102.add
+  sub = magic_calculation_102.sub
+
+  if a < b:
+    c = add(a, b)
+    for i in range(4, 6):
+      c = add(c, i)
+    return c
+  else:
+    return sub(a, b)
