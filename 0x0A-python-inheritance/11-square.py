21d590cf3d037b1b08e197cf184bbc40dc0dca20
simoonge1 <simoimagine@gmail.com>
2023-11-06 05:39:17 -0800

    py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-# 2-is_same_class.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a class-checking function."""
+"""
+more class base
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+Square class
+"""
+class Square(Rectangle):
+    """ Square Class """
+    def __init__(self, size):
+        """ size init"""
+        self.__size = size
+        super().__init__(self.__size, self.__size)
 
-
-def is_same_class(obj, a_class):
-    """Check if an object is exactly an instance of a given class.
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is exactly an instance of a_class - True.
-        Otherwise - False.
-    """
-    if type(obj) == a_class:
-        return True
-    return False
+    def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
